9667a48328d9c7c84034f6f1aa335d0ad5f33d11
Jakub Holler <jakub.holler1@gmail.com>
2023-08-14 15:02:42 +0200

    just save

--- adventure/static/adventure/index.js
+++ adventure/static/adventure/index.js
@@ -9,8 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const adventures = document.querySelectorAll('.adventure-item')
     adventures.forEach( adv => {
+        const children = adv.children;
+        const deleteImg = children[1];
+
+        //click to open adventure site
         adv.onclick = function() {
             window.location.href = "/adventure/" + adv.dataset.id;
+        }
+
+        deleteImg.onclick = function() {
+            console.log('test ikonki');
         }
     })
 })
