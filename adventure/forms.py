71864ac304a46c12e2082fa0336af2403b1cc43b
Jakub Holler <jakub.holler1@gmail.com>
2023-08-30 13:43:43 +0200

    form styling

--- adventure/forms.py
+++ adventure/forms.py
@@ -13,18 +13,18 @@ class EditDayForm(forms.Form):
     edit_day_description = forms.CharField(max_length=5000, widget=forms.Textarea(attrs={ 'autofocus': 'true', 'placeholder': 'YYYY-MM-DD'}) , strip=True,)
 
 class NewLocationForm(forms.Form):
-    new_location_name = forms.CharField(max_length=5000, strip=True, widget=forms.TextInput(attrs={'placeholder': 'Title', 'autofocus': 'true'}))
-    new_location_description = forms.CharField(max_length=5000, widget=forms.Textarea(attrs={'placeholder': 'Description',}) , strip=True,)
-    new_location_lat = forms.FloatField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
-    new_location_lng = forms.FloatField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
+    new_location_name = forms.CharField(label='Location', max_length=5000, strip=True, widget=forms.TextInput(attrs={'placeholder': 'Name', 'autofocus': 'true'}))
+    new_location_description = forms.CharField(label='Description',max_length=5000, widget=forms.Textarea(attrs={'placeholder': 'Description',}) , strip=True,)
+    new_location_lat = forms.FloatField(label='Coordinates (click on map)', widget=forms.NumberInput(attrs={'placeholder': 'latitude',}))
+    new_location_lng = forms.FloatField(label='', widget=forms.NumberInput(attrs={'placeholder': 'longitude',}))
     new_location_day = forms.IntegerField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
-    new_location_img = forms.ImageField(required=False)
+    new_location_img = forms.ImageField(label='Location photo',required=False)
 
 class EditLocationForm(forms.Form):
-    edit_location_name = forms.CharField(max_length=5000, strip=True, widget=forms.TextInput(attrs={'placeholder': 'Title', 'autofocus': 'true'}))
-    edit_location_description = forms.CharField(max_length=5000, widget=forms.Textarea(attrs={'placeholder': 'Description',}) , strip=True,)
+    edit_location_name = forms.CharField(max_length=5000, strip=True, label='Name', widget=forms.TextInput(attrs={'placeholder': 'Name', 'autofocus': 'true'}))
+    edit_location_description = forms.CharField(max_length=5000, label='Description', widget=forms.Textarea(attrs={'placeholder': 'Description',}) , strip=True,)
     # edit_location_lat = forms.FloatField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
     # edit_location_lng = forms.FloatField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
     # edit_location_day = forms.IntegerField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
     edit_location_id = forms.IntegerField(widget=forms.NumberInput(attrs={'type': 'hidden',}))
-    edit_location_img = forms.ImageField(required=False)
+    edit_location_img = forms.ImageField(required=False, label='Location photo')
