440b8101452f37b653430132c155c85ef8d7588c
Jakub Holler <jakub.holler1@gmail.com>
2023-09-22 08:15:40 +0200

    defined __str__ for models

--- adventure/models.py
+++ adventure/models.py
@@ -8,13 +8,18 @@ class Adventure(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     is_public = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'adv: {self.title}'
+
 
 class Day(models.Model):
     adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
     description = models.CharField(max_length=5000)
     date = models.DateField()
-    # locations = models.ManyToManyField(Location, related_name="location_of_day")
-    # routs = models.ManyToManyField(Rout, related_name="routs_of_day")
+
+    def __str__(self):
+        return f'day: {self.date}'
+
 
     
 class Location(models.Model):
@@ -25,3 +30,6 @@ class Location(models.Model):
     photo = models.ImageField(null=True, blank=True, upload_to='images/')
     lat = models.FloatField()
     lng = models.FloatField()
+
+    def __str__(self):
+        return self.name
